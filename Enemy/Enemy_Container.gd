1f5c62a159675f2a303b55a8ddaf19abaf382f63
astienin <astienin@iu.edu>
2022-11-02 10:13:16 -0700

    Project not Complete
    I should have a full game by this weekend

--- Enemy/Enemy_Container.gd
+++ Enemy/Enemy_Container.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var Bat = load("res://Enemy/Bat.tscn")
+onready var Bat2 = load("res://Enemy/Bat 2.gd")
 
 func _physics_process(_delta):
 	if not has_node("Bat"):
--- Global.gd
+++ Global.gd
@@ -1,5 +1,9 @@
 extends Node
 
+var lives = 5
+var score = 0 
+
+
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
